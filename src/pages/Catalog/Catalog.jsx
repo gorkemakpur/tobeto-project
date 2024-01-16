a3a9ffc4bde24ad42d44aec835f6dbd608f83eaf
gorkemakpur <gorkemakpur@gmail.com>
2024-01-16 17:03:58 +0300

     

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -30,7 +30,7 @@ export default function Catalog() {
       {/*sağdaki eğitim listesi*/}
       <div className="col-8">
         <Row>
-          <div className="col-4">
+          <div className="col-3">
             <Card
               style={{
                 width: "18rem",
@@ -42,6 +42,72 @@ export default function Catalog() {
               <Card.Img
                 variant="top"
                 src="https://tobeto.s3.cloud.ngn.com.tr/ENK_36573_a8546fa0ff.jpg"
+                className="edubox-img"
+                style={{ borderRadius: "15px" }}
+              />
+              <BsPlayCircleFill
+                color="#8014E4"
+                size={30}
+                style={{
+                  position: "absolute",
+                  top: "10px",
+                  right: "10px",
+                  cursor: "pointer",
+                }}
+              />
+
+              <Card.Body
+                style={{
+                  height:"100px",
+                  position: "absolute",
+                  bottom: "0",
+                  left: "0",
+                  width: "100%",
+                  backgroundColor: "rgba(65,65,65.5)",
+                  backdropFilter:"blur(21.5px)",
+                  color: "white",
+                  padding: "15px",
+                }}
+              >
+                <Row>
+                  <div
+                    className="col-12"
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Card.Text className="pro" style={{ marginLeft: "5px" }}>
+                      <BsPerson />
+                      Gürkan İlişen - <BsClock />
+                      4s 14dk
+                    </Card.Text>
+                  </div>
+                  <div className="col-12">
+                    <Card.Title
+                      className="name"
+                      style={{ marginTop: "10px", fontWeight: "bold" }}
+                    >
+                      Dinle, Anla, İfade Et: Etkili İletişim Gelişim Yolculuğu
+                    </Card.Title>
+                  </div>
+                </Row>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-3">
+            <Card
+              style={{
+                width: "18rem",
+                cursor: "pointer",
+                borderRadius: "15px",
+                position: "relative",
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src="https://tobeto.s3.cloud.ngn.com.tr/ENK_365670_08eb2bab1a.jpg"
                 className="edubox-img"
                 style={{ borderRadius: "15px" }}
               />
@@ -81,7 +147,7 @@ export default function Catalog() {
                     <Card.Text className="pro" style={{ marginLeft: "5px" }}>
                       <BsPerson />
                       Gürkan İlişen - <BsClock />
-                      4s 14dk
+                      40dk
                     </Card.Text>
                   </div>
                   <div className="col-12">
@@ -89,14 +155,81 @@ export default function Catalog() {
                       className="name"
                       style={{ marginTop: "10px", fontWeight: "bold" }}
                     >
-                      Dinle, Anla, İfade Et: Etkili İletişim Gelişim Yolculuğu
+                      Sürdürülebilir Bir Dünya için Bireysel Farkındalık
                     </Card.Title>
                   </div>
                 </Row>
               </Card.Body>
             </Card>
           </div>
-          <div></div>
+          <div className="col-3">
+            <Card
+              style={{
+                width: "18rem",
+                cursor: "pointer",
+                borderRadius: "15px",
+                position: "relative",
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src="https://tobeto.s3.cloud.ngn.com.tr/ENK_365693_fd2d22fa3a.jpg"
+                className="edubox-img"
+                style={{ borderRadius: "15px" }}
+              />
+              <BsPlayCircleFill
+                color="#8014E4"
+                size={30}
+                style={{
+                  position: "absolute",
+                  top: "10px",
+                  right: "10px",
+                  cursor: "pointer",
+                }}
+              />
+
+              <Card.Body
+                style={{
+                  height:"100px",
+                  position: "absolute",
+                  bottom: "0",
+                  left: "0",
+                  width: "100%",
+                  backgroundColor: "rgba(65,65,65,.5)",
+                  backdropFilter:"blur(21.5px)",
+                  color: "white",
+                  padding: "15px",
+                }}
+              >
+                <Row>
+                  <div
+                    className="col-12"
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Card.Text className="pro" style={{ marginLeft: "5px" }}>
+                      <BsPerson />
+                      Gürkan İlişen - <BsClock />
+                      53dk
+                    </Card.Text>
+                  </div>
+                  <div className="col-12">
+                    <Card.Title
+                      className="name"
+                      style={{ marginTop: "10px", fontWeight: "bold" }}
+                    >
+                      Hibrit Yaşamda Duyguyu Düzenleme
+                    </Card.Title>
+                  </div>
+                </Row>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-3"></div>
+          
         </Row>
       </div>
       {/*sağdaki eğitim listesi bitiş*/}

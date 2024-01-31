a1792354dc3f9de35b7533e9fe34e6dda303a71e
gorkemakpur <gorkemakpur@gmail.com>
2024-01-31 18:02:44 +0300

    Co-authored-by: umut güney <umudguny@users.noreply.github.com>

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,66 +1,105 @@
-import React from "react";
 import "./Login.css";
 import { Button, Col, Image, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../../store/reducers/userReducer";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  //states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //redux state
+  const { loading, error } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLoginEvent = (e) => {
+    e.preventDefault();
+    let userCredentials = {
+      email,
+      password,
+    };
+    dispatch(loginUser(userCredentials)).then((result) => {
+      if (result.payload) {
+        setEmail("");
+        setPassword("");
+        navigate("/");
+      }
+    });
+    
+  };
+
   return (
     <>
-        <Row className="p-5 mb-5">
+      <Row className="p-5 mb-5">
         <Col lg={3} md={6} sm={12}></Col>
-        
+
         <Col lg={3} md={6} sm={12}>
-        <div className="login-main">
-        <div className="login card-login-animation">
-          <div className="form">
-            <form noValidate>
-              <Image
-                src="https://tobeto.com/_next/static/media/tobeto-logo.29b55e1c.svg"
-                width={"120px"}
-                className="mb-3"
-              ></Image>
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter email id / username"
-                className="form-control inp_text"
-                id="email"
-              />
+          <div className="login-main">
+            <div className="login card-login-animation">
+              <div className="form">
+                <form noValidate onSubmit={handleLoginEvent}>
+                  <Image
+                    src="https://tobeto.com/_next/static/media/tobeto-logo.29b55e1c.svg"
+                    width={"120px"}
+                    className="mb-3"
+                  ></Image>
+                  <input
+                    type="email"
+                    name="email"
+                    required
+                    placeholder="Enter email id / username"
+                    className="form-control inp_text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
 
-              <input
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                className="form-control"
-              />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
 
-              <button class="btn btn-primary w-100 mt-6">Login</button>
-            </form>
-          </div>
-        </div>
-        </div>
-        </Col>
-        <Col lg={3} md={6} sm={12}>
-        <div className="login card-right">
-
-          <div className="form">
-          <div className="text-center mt-5">
-              <Image
-                src="https://tobeto.com/_next/static/media/ik-logo-dark.7938c0de.svg"
-                width={"100px"}
-              ></Image>
-              <hr />
-              <div className="right-card-bottom-logo">
-              <span className="rigth-card-job">Aradığın "İş" Burada!</span>
-              <br/>
-              <Button className="btn-sm">Başvur</Button>
+                  <button className="btn btn-primary w-100 mt-6">
+                    {loading ? "Loading..." : "Login"}
+                  </button>
+                  {error && (
+                    <div className="alert alert-danger" role="alert">
+                      {error}
+                    </div>
+                  )}
+                </form>
               </div>
             </div>
           </div>
-
-        </div>
+        </Col>
+        <Col lg={3} md={6} sm={12}>
+          <div className="login card-right">
+            <div className="form">
+              <div className="text-center mt-5">
+                <Image
+                  src="https://tobeto.com/_next/static/media/ik-logo-dark.7938c0de.svg"
+                  width={"100px"}
+                ></Image>
+                <hr />
+                <div className="right-card-bottom-logo">
+                  <span className="rigth-card-job">Aradığın "İş" Burada!</span>
+                  <br />
+                  <Button className="btn-sm">Başvur</Button>
+                </div>
+              </div>
+            </div>
+          </div>
         </Col>
         <div className="col-3"></div>
-        
-        </Row>
+      </Row>
     </>
   );
 }
---
+++ src/store/reducers/userReducer.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  async (userCredentials) => {
+    const request = await axios.post(
+      "https://localhost:44340/api/Auth/login",
+      userCredentials
+    );
+    const response = await request.data.data;
+    localStorage.setItem("user", JSON.stringify(response));
+    return response;
+  }
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    loading: false,
+    user: null,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.user = null;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+        state.error = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.user = null;
+        console.log(action.error.message);
+        if (action.error.message === "Request failed with status code 401") {
+          state.error = "Access Denied! Invalid Credentials";
+        } else {
+          state.error = action.error.message;
+        }
+      });
+  },
+});
+export default userSlice.reducer;
